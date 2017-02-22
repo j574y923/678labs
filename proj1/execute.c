a797fdbf5c12e0a5ae8c1196db0367a5a8f32d71
j574y923 <j574y923@ku.edu>
2017-02-21 22:08:49 -0600

    naveed, multiple pipes

--- proj1/execute.c
+++ proj1/execute.c
@@ -13,13 +13,18 @@
 
 #include "quash.h"
 
-#include <limits.h>
-#include <unistd.h>
+#include <sys/wait.h>
+
+#include <sys/stat.h>
+
 #include <fcntl.h>
 
-int fd[2];
-// bool p_exists;
+#include <unistd.h>
 
+#include "pid_queue.h"
+
+int idx;
+int idx_num;
 // Remove this and all expansion calls to it
 /**
  * @brief Note calls to any function that requires implementation
@@ -35,19 +40,18 @@ int fd[2];
 char* get_current_directory(bool* should_free) {
   // TODO: Get the current working directory. This will fix the prompt path.
   // HINT: This should be pretty simple
-  // IMPLEMENT_ME();
+  //IMPLEMENT_ME();
+  char *buf;
 
+  buf=(char *)malloc(100*sizeof(char));
+  getcwd(buf,1024);
+
+  //printf("current_directory:%s\n",buf);
+  //free(buf);
   // Change this to true if necessary
-  *should_free = false;
+  *should_free = true;
 
-  char cwd[1024];
-  char *cwd_ptr = malloc(1024);
-  if ((cwd_ptr = getcwd(cwd, sizeof(cwd))) != NULL) {}
-      // fprintf(stdout, "Current working dir: %s\n", cwd);
-  else
-      perror("getcwd() error");
-
-  return getcwd(NULL, 1024);
+  return buf;
 }
 
 // Returns the value of an environment variable env_var
@@ -56,10 +60,11 @@ const char* lookup_env(const char* env_var) {
   // to interpret variables from the command line and display the prompt
   // correctly
   // HINT: This should be pretty simple
-  // IMPLEMENT_ME();
-
+  //IMPLEMENT_ME();
+  
   // TODO: Remove warning silencers
-  // (void) env_var; // Silence unused variable warning
+  (void) env_var; // Silence unused variable warning
+
   return getenv(env_var);
 }
 
@@ -68,7 +73,7 @@ void check_jobs_bg_status() {
   // TODO: Check on the statuses of all processes belonging to all background
   // jobs. This function should remove jobs from the jobs queue once all
   // processes belonging to a job have completed.
-  IMPLEMENT_ME();
+  //IMPLEMENT_ME();
 
   // TODO: Once jobs are implemented, uncomment and fill the following line
   // print_job_bg_complete(job_id, pid, cmd);
@@ -106,14 +111,13 @@ void run_generic(GenericCommand cmd) {
   char** args = cmd.args;
 
   // TODO: Remove warning silencers
-  // (void) exec; // Silence unused variable warning
-  // (void) args; // Silence unused variable warning
+  //(void) exec; // Silence unused variable warning
+  //(void) args; // Silence unused variable warning
 
   // TODO: Implement run generic
-  // IMPLEMENT_ME();
-
-  if(execvp(exec, args) < 0)
-    perror("ERROR: Failed to execute program");
+  //IMPLEMENT_ME();
+  execvp(exec, args);
+  //perror("ERROR: Failed to execute program");
 }
 
 // Print strings
@@ -123,14 +127,15 @@ void run_echo(EchoCommand cmd) {
   char** str = cmd.args;
 
   // TODO: Remove warning silencers
-  // (void) str; // Silence unused variable warning
+  //(void) str; // Silence unused variable warning
 
   // TODO: Implement echo
-  // IMPLEMENT_ME();
-  for(; *str != NULL; ++str)
+  //IMPLEMENT_ME();
+  while(*str != NULL){
     printf("%s ",*str);
+    str++;
+  }  
   printf("\n");
-
   // Flush the buffer before returning
   fflush(stdout);
 }
@@ -142,42 +147,38 @@ void run_export(ExportCommand cmd) {
   const char* val = cmd.val;
 
   // TODO: Remove warning silencers
-  // (void) env_var; // Silence unused variable warning
-  // (void) val;     // Silence unused variable warning
+  (void) env_var; // Silence unused variable warning
+  (void) val;     // Silence unused variable warning
 
   // TODO: Implement export.
   // HINT: This should be quite simple.
-  // IMPLEMENT_ME();
-  setenv(env_var, val, 1);
+  //IMPLEMENT_ME();
+  if(setenv(env_var, val, 1) != 0)
+	  perror("ERROR: Failed to export environment variable\n");
 }
 
 // Changes the current working directory
 void run_cd(CDCommand cmd) {
   // Get the directory name
   const char* dir = cmd.dir;
-
+  
   // Check if the directory is valid
   if (dir == NULL) {
     perror("ERROR: Failed to resolve path");
     return;
   }
 
-  // Change directory
-  char path_buf[PATH_MAX];
-  char *real_path_ptr = realpath(dir, path_buf);
-
-  chdir(dir);// chdir(real_path_ptr);
-
+  // TODO: Change directory
+  chdir(cmd.dir);
   // TODO: Update the PWD environment variable to be the new current working
   // directory and optionally update OLD_PWD environment variable to be the old
   // working directory.
-  // IMPLEMENT_ME();
-  // bool *idk;
-  // lookup_env("OLD_PWD") = lookup_env("PWD");
-  // lookup_env("PWD") = get_current_directory(idk);
-  bool *idk = malloc(sizeof(bool));  
-  setenv("OLD_PWD", lookup_env("PWD"), 1);
-  setenv("PWD", dir, 1);
+  const char* old_dir = getenv("PWD");
+  if(setenv("PWD", dir, 1) != 0)				//setenv returns -1 if unsuccessfull, 0 for success
+	  perror("ERROR: Failed to update PWD");
+  if(setenv("OLD_PWD", old_dir, 1) != 0)
+	  perror("ERROR: Failed to update OLD_PWD"); 
+  //IMPLEMENT_ME();
 }
 
 // Sends a signal to all processes contained in a job
@@ -197,12 +198,15 @@ void run_kill(KillCommand cmd) {
 // Prints the current working directory to stdout
 void run_pwd() {
   // TODO: Print the current working directory
-  // IMPLEMENT_ME();
+  //IMPLEMENT_ME();
+  char *buf;
+  //buf=(char *)malloc(100*sizeof(char));
+  buf = (char*)get_current_dir_name();
+  //getcwu(buf,1024);
 
+  printf("%s\n",buf);
   // Flush the buffer before returning
-  char cwd[1024];
-  if (getcwd(cwd, sizeof(cwd)) != NULL)
-      printf("%s\n", cwd);
+  free(buf);
   fflush(stdout);
 }
 
@@ -319,7 +323,6 @@ void parent_run_command(Command cmd) {
  */
 void create_process(CommandHolder holder) {
   // Read the flags field from the parser
-
   bool p_in  = holder.flags & PIPE_IN;
   bool p_out = holder.flags & PIPE_OUT;
   bool r_in  = holder.flags & REDIRECT_IN;
@@ -328,69 +331,97 @@ void create_process(CommandHolder holder) {
                                                // is true
 
   // TODO: Remove warning silencers
-  // (void) p_in;  // Silence unused variable warning
-  // (void) p_out; // Silence unused variable warning
-  // (void) r_in;  // Silence unused variable warning
-  // (void) r_out; // Silence unused variable warning
-  // (void) r_app; // Silence unused variable warning
+  (void) p_in;  // Silence unused variable warning
+  (void) p_out; // Silence unused variable warning
+  (void) r_in;  // Silence unused variable warning
+  (void) r_out; // Silence unused variable warning
+  (void) r_app; // Silence unused variable warning
 
   // TODO: Setup pipes, redirects, and new process
-  // IMPLEMENT_ME();
-  // int fd[2];
-  int f_in;
-  pipe(fd);
-  pid_t pid;
-  pid = fork();
-  if(pid == 0) {
+  //IMPLEMENT_ME();
 
-    if(p_in){
-      dup2(fd[0], STDIN_FILENO);
-      close(fd[1]);
-    }
-    else if(p_out){
-      dup2(fd[1], STDOUT_FILENO);
-      close(fd[0]);
-    }
-    else{
-      close(fd[0]);
-      close(fd[1]);
-    }
+  pid_t child_pid;
+  int status; 				// Declare status of child pid
 
+  //int fd_a[2];				// File descriptor for pipe handle
+  //pipe(fd_a);
+
+  int pipe_end = idx_num * 2;
+
+  child_pid = fork();
+  
+  if(child_pid == 0){
+ 
+    if(p_in && !p_out){
+      printf("CHILD: P_IN: STDIN\n");	
+      dup2(fd_a[idx * 2 - 2], STDIN_FILENO);	// Replace the standard input with pipe read end
+      close(fd_a[idx * 2 - 1]);
+
+      //close unused pipes
+      for(int i = 0; i < idx * 2 - 2; ++i)
+        close(fd_a[i]);
+    } else if(p_out && !p_in){
+      printf("CHILD: P_OUT: STDOUT\n");
+      dup2(fd_a[1], STDOUT_FILENO);	// Replace the standard output with pipe write end
+      close(fd_a[0]);
+
+      //close unused pipes
+      for(int i = 2; i < idx * 2; ++i)
+        close(fd_a[i]);
+    } else if (p_out && p_in){
+      printf("CHILD: P_OUT && P_IN\n");
+      dup2(fd_a[pipe_end-2], STDIN_FILENO);
+      close(fd_a[pipe_end-1]);
+
+      dup2(fd_a[pipe_end+1], STDOUT_FILENO);
+      close(fd_a[pipe_end]);
+
+      //close unused pipes
+      for(int i = 0; i < pipe_end-2; ++i)
+        close(fd_a[i]);
+      for(int i = pipe_end+2; i < idx * 2; ++i)
+        close(fd_a[i]);
+    }
+    
     if(r_in){
-
-      f_in = open(holder.redirect_in, O_RDONLY);
-      dup2(f_in, STDIN_FILENO);
-      close(f_in);
-
+      int fp = open(holder.redirect_in, O_RDONLY);
+      dup2(fp, 0);
+      close(fp);
     }
-    else if(r_out){
-      if(r_app) {
-        f_in = open(holder.redirect_out, O_APPEND | O_WRONLY | O_CREAT, 0777 );
-        dup2(f_in, STDOUT_FILENO);
-        close(f_in);
-      } 
+
+    if(r_out){
+      if(r_app){
+        int fp = open(holder.redirect_out, O_RDWR | O_CREAT | O_APPEND, 0777);
+        dup2(fp, 1);
+        close(fp);
+      }
       else{
-        f_in = open(holder.redirect_out, O_WRONLY | O_CREAT, 0777 );
-        dup2(f_in, STDIN_FILENO);
-        close(f_in);
+        int fp = open(holder.redirect_out, O_RDWR | O_CREAT, 0777);
+        dup2(fp, 1);
+        close(fp);
       }
     }
-
-    child_run_command(holder.cmd); // This should be done in the child branch of a fork
-
+    child_run_command(holder.cmd); // This should be done in the child branch 
     exit(0);
   }
-  else {
-    close(fd[0]);
-    close(fd[1]);
-
-    parent_run_command(holder.cmd); // This should be done in the parent branch of
-                                    // a fork
+  else{
+    push_back_pid_queue (&process_queue, child_pid);	
+    parent_run_command(holder.cmd); // This should be done in the parent branch of a fork
   }
+
+  //close(fd_a[0]);
+  //close(fd_a[1]);
+
+  /*if ((waitpid(child_pid, &status, 0)) == -1) {
+  	perror("Process 1 encountered an error");
+  	return;
+  }*/
 }
 
 // Run a list of commands
 void run_script(CommandHolder* holders) {
+  int status;
+
   if (holders == NULL)
     return;
 
@@ -404,21 +435,64 @@ void run_script(CommandHolder* holders) {
 
   CommandType type;
 
+  idx = 0;
+  for (idx; (type = get_command_holder_type(holders[idx])) != EOC; ++idx){}
+  printf("IDX???%d\n",idx);
+
+  --idx;//represents the number of pipes
+  if(idx < 1)
+    idx = 1;
+
+  fd_a = (int *)malloc(idx * 2 * sizeof(int));//need 2 ends for each pipe (denoted by idx)
+
+  idx_num = 0;//keep track of which pipe we are at
+
+  //make pipes
+  for (int i = 0; i < idx; ++i) 
+    pipe(&fd_a[i*2]);
+
   // Run all commands in the `holder` array
-  for (int i = 0; (type = get_command_holder_type(holders[i])) != EOC; ++i)
+  for (int i = 0; (type = get_command_holder_type(holders[i])) != EOC; ++i){
     create_process(holders[i]);
+    printf("IDX_NUM???%d\n",idx_num);
+    ++idx_num;
+  }
+
+  //close pipes
+  for(int i = 0; i < idx; ++i){
+    close(fd_a[i*2]);
+    close(fd_a[i*2+1]);
+  }
+
+  free(fd_a);
+  fd_a = NULL;
+
+  int fg_pid;
+  job_struct js;
 
   if (!(holders[0].flags & BACKGROUND)) {
     // Not a background Job
     // TODO: Wait for all processes under the job to complete
-    IMPLEMENT_ME();
+    //IMPLEMENT_ME();
+    while(is_empty_pid_queue (&process_queue) == false){
+      fg_pid = pop_back_pid_queue(&process_queue);  
+      waitpid(fg_pid, &status, 0);
+    }
   }
   else {
     // A background job.
+    bg_jobs_count++;
     // TODO: Push the new job to the job queue
-    IMPLEMENT_ME();
+    //IMPLEMENT_ME();
+    int bg_pid;
+    bg_pid = pop_front_pid_queue (&process_queue);
 
+    js.job_id = bg_jobs_count;
+    js.process_queue = &process_queue;
+
+    push_back_job_queue (&bg_jobs_queue, js);
     // TODO: Once jobs are implemented, uncomment and fill the following line
-    // print_job_bg_start(job_id, pid, cmd);
+    //char *cmd = strchr(get_command_string(), ' ');
+    print_job_bg_start(js.job_id, bg_pid, "TODO");
   }
 }
